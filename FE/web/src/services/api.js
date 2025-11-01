63da726fcb8c7f43ed7b4d39bbdfe339acd4f69a
Nguyen-Thiet-Do-Tw1 <127604916+Nguyen-Thiet-Do@users.noreply.github.com>
2025-11-01 18:58:27 +0700

    fix upload ebook

--- FE/web/src/services/api.js
+++ FE/web/src/services/api.js
@@ -5,7 +5,7 @@ const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
-  timeout: 10000,
+  timeout: 0,
   headers: { "Content-Type": "application/json" },
 });
 
